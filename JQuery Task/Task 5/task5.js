1b8b83150bbe615f67eb7d0be330538efae4ed64
manishinnoraft <118262812+manishinnoraft@users.noreply.github.com>
2022-12-05 15:09:29 +0530

    Add files via upload

---
+++ JQuery Task/Task 5/task5.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('footer').click(function () {
+        $('.popup').css('display', 'block');
+    });
+    $('#crossit').click(function () {
+        $('.popup').css('display', 'none');
+    });
+}
+);
